0f1ff0af3b64a1ffee74737afae61c8dd94f8362
Christopher Stanley <cbstanley12@gmail.com>
2019-05-26 08:03:07 -0600

    feat: grid styling for calculator layout

--- src/App.js
+++ src/App.js
@@ -12,34 +12,34 @@ class Calculator extends Component {
 
   render() {
     return (
-      <div>
+      <div class="container">
         <Display equation={this.state.equation} display={this.state.display} />
-        <Button id="clear" value="clear" display="AC" />
-        <Button id="sign" value="+/-" display="±" />
-        <Button id="percent" value="%" display="%" />
-        <Button id="divide" value="/" display="÷" />
-        <Button id="seven" value="7" display="7" />
-        <Button id="eight" value="8" display="8" />
-        <Button id="nine" value="9" display="9" />
-        <Button id="multiply" value="*" display="×" />
-        <Button id="four" value="4" display="4" />
-        <Button id="five" value="5" display="5" />
-        <Button id="six" value="6" display="6" />
-        <Button id="subtract" value="-" display="−" />
-        <Button id="one" value="1" display="1" />
-        <Button id="two" value="2" display="2" />
-        <Button id="three" value="3" display="3" />
-        <Button id="add" value="+" display="+" />
-        <Button id="zero" value="0" display="0" />
-        <Button id="decimal" value="." display="." />
-        <Button id="equals" value="=" display="=" />
+        <Button id="clear" value="clear" display="AC" class="row-3 col-1" />
+        <Button id="sign" value="+/-" display="±" class="row-3 col-2" />
+        <Button id="percent" value="%" display="%" class="row-3 col-3" />
+        <Button id="divide" value="/" display="÷" class="row-3 col-4" />
+        <Button id="seven" value="7" display="7" class="row-4 col-1" />
+        <Button id="eight" value="8" display="8" class="row-4 col-2" />
+        <Button id="nine" value="9" display="9" class="row-4 col-3" />
+        <Button id="multiply" value="*" display="×" class="row-4 col-4" />
+        <Button id="four" value="4" display="4" class="row-5 col-1" />
+        <Button id="five" value="5" display="5" class="row-5 col-2" />
+        <Button id="six" value="6" display="6" class="row-5 col-3" />
+        <Button id="subtract" value="-" display="−" class="row-5 col-4" />
+        <Button id="one" value="1" display="1" class="row-6 col-1" />
+        <Button id="two" value="2" display="2" class="row-6 col-2" />
+        <Button id="three" value="3" display="3" class="row-6 col-3" />
+        <Button id="add" value="+" display="+" class="row-6 col-4" />
+        <Button id="zero" value="0" display="0" class="row-7 col-1-2" />
+        <Button id="decimal" value="." display="." class="row-7 col-3" />
+        <Button id="equals" value="=" display="=" class="row-7 col-4" />
       </div>
     );
   }
 }
 
-const Display = props => <div id="calc-display"><span id="eq">{props.equation}</span><span id="dis">{props.display}</span></div>;
+const Display = props => <div id="calc-display" className="row-1-2 col-1-4"><span id="eq">{props.equation}</span><span id="dis">{props.display}</span></div>;
 
-const Button = props => <button type="button" id={props.id} value={props.value}>{props.display}</button>;
+const Button = props => <button type="button" id={props.id} value={props.value} className={props.class}>{props.display}</button>;
 
 export default Calculator;
